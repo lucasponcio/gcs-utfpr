3a77f7fd5c5f72351fc5b346d99bd12b788b9b87
lucasponcio <lucasponcio@alunos.utfpr.edu.br>
2026-06-14 17:36:28 -0300

    Venda.java

---
+++ Venda.java
@@ -0,0 +1,9 @@
+public class Venda {
+
+    private int numeroVenda;
+    private double valorTotal;
+
+    public void calcularTotal() {
+        System.out.println("Total da venda: R$ " + valorTotal);
+    }
+}
